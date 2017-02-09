6c1b67bb54d2848e292c98e8a1e5318e2c37aa19
monk1982 <hoangthuc2013@gmail.com>
2017-02-09 17:32:41 +0700

    Update exam.js

--- Declaring/exam.js
+++ Declaring/exam.js
@@ -20,7 +20,7 @@ console.log('original topic = ' + topic);
 // Bên dưới là ví dụ:
 // Nhóm 5 ô đỏ sẽ không dùng let và cho thông báo thứ tự các div
 // Nhóm 5 ô xanh dương sẽ dùng let và cho thông báo thứ tự các div
-var div, container = document.getElementById('content')
+var div, container = document.getElementById('container')
 for (var i=0; i<5; i++){
   div= document.createElement('div')
   div.onclick = function() {
