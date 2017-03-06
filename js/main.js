374c3dbed989b8313fca03acd7b4b0fab6278ef2
monk1982 <hoangthuc2013@gmail.com>
2017-03-06 16:14:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// Initializes FriendlyChat.
+function quiz() {
+  this.checkSetup();  
+  this.initFirebase();
+}
+
 // Sets up shortcuts to Firebase features and initiate firebase auth.
 quiz.prototype.initFirebase = function() {
   // Shortcuts to Firebase SDK features.
@@ -49,4 +55,23 @@ quiz.prototype.checkSignedInWithMessage = function() {
   if (this.auth.currentUser) {
     return true;
   }
+};
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+quiz.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions.');
+  } else if (config.storageBucket === '') {
+    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+        'actually a Firebase bug that occurs rarely. ' +
+        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+        'and make sure the storageBucket attribute is not empty. ' +
+        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
+        'displayed there.');
+  }
+};
+
+window.onload = function() {
+  window.quiz = new quiz();
 };
