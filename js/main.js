5508a07ee3c0b9025e324d1e5016c79ca13189a5
monk1982 <hoangthuc2013@gmail.com>
2017-03-06 16:06:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,44 @@ quiz.prototype.initFirebase = function() {
   this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
+// Signs-in Firebase.
+quiz.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GithubAuthProvider();
+  this.auth.signInWithPopup(provider).then(function(result) {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    var token = result.credential.accessToken;    
+    var user = result.user;
+    console.log('User='+user);    
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;    
+    var email = error.email;    
+    var credential = error.credential;
+    console.log('Err='+errorMessage);
+  });
+};
+
+// Signs-out of Firebase.
+quiz.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+quiz.prototype.onAuthStateChanged = function(user) {
+  if (user) { // User is signed in!
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = user.photoURL; // Only change these two lines!
+    var userName = user.displayName;   // Only change these two lines!
+    console.log(user.displayName);
+};
+  
+// Returns true if user is signed-in. Otherwise false and displays a message.
+quiz.prototype.checkSignedInWithMessage = function() {
+  // Return true if the user is signed in Firebase
+  if (this.auth.currentUser) {
+    return true;
+  }
+};
