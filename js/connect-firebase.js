1d8a99c67d152a2b5c8cf7672b8457fa8db8d688
monk1982 <hoangthuc2013@gmail.com>
2017-03-06 15:18:32 +0700

    Update connect-firebase.js

--- js/connect-firebase.js
+++ js/connect-firebase.js
@@ -1,10 +1,6 @@
 // Set the configuration for your app  
-console.log('Run first');
   var config = {
-    apiKey: "KvujMTzIGrZZvGe26wyNOlRcghZ5iysTdMcSiusr",
-    authDomain: "intense-torch-7295.firebaseapp.com",
-    databaseURL: "https://intense-torch-7295.firebaseio.com",
-    storageBucket: "intense-torch-7295.appspot.com"
+    databaseURL: "https://intense-torch-7295.firebaseio.com",    
   };
   firebase.initializeApp(config);
 
